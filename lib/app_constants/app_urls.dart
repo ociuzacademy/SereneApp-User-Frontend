cb72d51380f95c7f337321300ad4b0a7ea7c8102
Mikhil Mohan C <mikhilmohan13@gmail.com>
2026-09-21 12:47:17 +0530

    fix: update base URL for API endpoints

--- lib/app_constants/app_urls.dart
+++ lib/app_constants/app_urls.dart
@@ -1,5 +1,5 @@
 class AppUrls {
-  static const String baseUrl = "https://vqp6fbbv-8001.inc1.devtunnels.ms";
+  static const String baseUrl = "https://417sptdw-8006.inc1.devtunnels.ms";
   static const String userAppUrl = "$baseUrl/userapp";
 
   static const String userRegisterUrl = "$userAppUrl/register/";
